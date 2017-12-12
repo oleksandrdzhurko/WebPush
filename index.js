be97202b39b2e68918c7a160b94aad6b0166563c
Oleksandr Dzhurko <odzhurko@pgs-soft.com>
2017-12-12 14:23:25 +0100

    first

---
+++ index.js
@@ -0,0 +1,89 @@
+// [Working example](/push-subscription-management_demo.html).
+if (!('serviceWorker' in navigator)) { console.log('Service Worker isn\'t supported on this browser, disable or hide UI. return');}
+
+if (!('PushManager' in window)) { console.log('Push isn\'t supported on this browser, disable or hide UI. return');}
+var subscriptionButton = document.getElementById('subscriptionButton');
+
+// As subscription object is needed in few places let's create a method which
+// returns a promise.
+function getSubscription() {
+  return navigator.serviceWorker.ready
+    .then(function(registration) {
+      return registration.pushManager.getSubscription();
+    });
+}
+
+// Register service worker and check the initial subscription state.
+// Set the UI (button) according to the status.
+if ('serviceWorker' in navigator) {
+  navigator.serviceWorker.register('service-worker.js')
+    .then(function() {
+      console.log('service worker registered');
+      subscriptionButton.removeAttribute('disabled');
+    }).catch(function(err) {
+        console.error('Unable to register service worker.', err);
+      });
+  getSubscription()
+    .then(function(subscription) {
+      if (subscription) {
+        console.log('Already subscribed', subscription.endpoint);
+        setUnsubscribeButton();
+      } else {
+        setSubscribeButton();
+      }
+    });
+}
+
+// Get the `registration` from service worker and create a new
+// subscription using `registration.pushManager.subscribe`. Then
+// register received new subscription by sending a POST request with its
+// endpoint to the server.
+function subscribe() {
+  navigator.serviceWorker.ready.then(function(registration) {
+    return registration.pushManager.subscribe({ userVisibleOnly: true });
+  }).then(function(subscription) {
+    console.log('Subscribed', subscription.endpoint);
+    return fetch('register', {
+      method: 'post',
+      headers: {
+        'Content-type': 'application/json'
+      },
+      body: JSON.stringify({
+        endpoint: subscription.endpoint
+      })
+    });
+  }).then(setUnsubscribeButton);
+}
+
+// Get existing subscription from service worker, unsubscribe
+// (`subscription.unsubscribe()`) and unregister it in the server with
+// a POST request to stop sending push messages to
+// unexisting endpoint.
+function unsubscribe() {
+  getSubscription().then(function(subscription) {
+    return subscription.unsubscribe()
+      .then(function() {
+        console.log('Unsubscribed', subscription.endpoint);
+        return fetch('unregister', {
+          method: 'post',
+          headers: {
+            'Content-type': 'application/json'
+          },
+          body: JSON.stringify({
+            endpoint: subscription.endpoint
+          })
+        });
+      });
+  }).then(setSubscribeButton);
+}
+
+// Change the subscription button's text and action.
+function setSubscribeButton() {
+  subscriptionButton.onclick = subscribe;
+  subscriptionButton.textContent = 'Subscribe!';
+}
+
+function setUnsubscribeButton() {
+  subscriptionButton.onclick = unsubscribe;
+  subscriptionButton.textContent = 'Unsubscribe!';
+}
---
+++ webnotifications.js
@@ -0,0 +1,48 @@
+if (!('serviceWorker' in navigator)) { console.log('Service Worker isn\'t supported on this browser, disable or hide UI. return');}
+
+if (!('PushManager' in window)) { console.log('Push isn\'t supported on this browser, disable or hide UI. return');}
+
+
+function registerServiceWorker() {
+    return navigator.serviceWorker.register('service-worker.js')
+    .then(function(registration) {
+      console.log('Service worker successfully registered.');
+      return registration;
+    })
+    .catch(function(err) {
+      console.error('Unable to register service worker.', err);
+    });
+  }
+
+
+  function askPermission() {
+    return new Promise(function(resolve, reject) {
+      const permissionResult = Notification.requestPermission(function(result) {
+        resolve(result);
+      });
+  
+      if (permissionResult) {
+        permissionResult.then(resolve, reject);
+      }
+    })
+    .then(function(permissionResult) {
+      if (permissionResult !== 'granted') {
+        throw new Error('We weren\'t granted permission.');
+      }
+    });
+  }
+
+
+var serviceWorker = registerServiceWorker();
+var permission = askPermission().then(permissionCallback);
+
+function permissionCallback(permission){
+    var data = {
+        msg: "Hello everyone"
+    };
+    
+    var e = new Notification("Test Title",{
+        body: "This is the body " + data.msg,
+        img: "https://static.volotea.com/imgr/logo/logo-en.png"
+    });
+}
