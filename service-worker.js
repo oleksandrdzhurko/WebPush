87cb8847ce8da13e34a212f98b6f3de00ba01f9d
Oleksandr Dzhurko <odzhurko@pgs-soft.com>
2017-12-13 14:41:23 +0100

    modify sw

--- service-worker.js
+++ service-worker.js
@@ -3,8 +3,10 @@
 // Listen to `push` notification event. Define the text to be displayed
 // and show the notification.
 self.addEventListener('push', function(event) {
-    event.waitUntil(self.registration.showNotification('ServiceWorker Cookbook', {
-      body: 'Push Notification Subscription Management'
+  var json = event.data.json();
+
+    event.waitUntil(self.registration.showNotification(json.notification.title, {
+      body: json.notification.body
     }));
   });
   
